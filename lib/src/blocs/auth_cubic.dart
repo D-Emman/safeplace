7693b141aa2fb6dfd72c5ac3ac202de242a1a425
D-Emman <102307238+D-Emman@users.noreply.github.com>
2025-10-10 18:13:32 +0100

    working

---
+++ lib/src/blocs/auth_cubic.dart
@@ -0,0 +1,78 @@
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import '../services/auth_service.dart';
+
+
+part 'auth_state.dart';
+
+
+class AuthCubit extends Cubit<AuthState> {
+  final AuthService _service = AuthService();
+
+
+  AuthCubit() : super(AuthInitial());
+
+
+  Future<void> checkAuth() async {
+    final user = FirebaseAuth.instance.currentUser;
+    if (user != null) emit(Authenticated(user));
+    else emit(Unauthenticated());
+  }
+
+
+  Future<void> register(String email, String password) async {
+    try {
+      emit(AuthLoading());
+      final cred = await _service.registerWithEmail(email: email, password: password);
+      emit(Authenticated(cred.user!));
+    } catch (e) {
+      emit(AuthError(e.toString()));
+      emit(Unauthenticated());
+    }
+  }
+
+
+  Future<void> login(String email, String password) async {
+    try {
+      emit(AuthLoading());
+      final cred = await _service.login(email: email, password: password);
+      emit(Authenticated(cred.user!));
+    } catch (e) {
+      emit(AuthError(e.toString()));
+      emit(Unauthenticated());
+    }
+  }
+
+
+  Future<void> logout() async {
+    await _service.logout();
+    emit(Unauthenticated());
+  }
+}
+
+
+//part of 'auth_cubit.dart';
+
+
+abstract class AuthState extends Equatable {
+  @override
+  List<Object?> get props => [];
+}
+
+
+class AuthInitial extends AuthState {}
+class AuthLoading extends AuthState {}
+class Authenticated extends AuthState {
+  final User user;
+  Authenticated(this.user);
+  @override
+  List<Object?> get props => [user];
+}
+class Unauthenticated extends AuthState {}
+class AuthError extends AuthState {
+  final String message;
+  AuthError(this.message);
+  @override
+  List<Object?> get props => [message];
+}
---
+++ lib/src/models/post_model.dart
@@ -0,0 +1,45 @@
+class PostModel {
+  final String id;
+  final String userId;
+  final String username;
+  final String content;
+  final List<String> tags;
+  final int supportCount;
+  final DateTime timestamp;
+  final bool isReported;
+
+
+  PostModel({
+    required this.id,
+    required this.userId,
+    required this.username,
+    required this.content,
+    required this.tags,
+    required this.supportCount,
+    required this.timestamp,
+    required this.isReported,
+  });
+
+
+  factory PostModel.fromMap(String id, Map<String, dynamic> map) => PostModel(
+    id: id,
+    userId: map['userId'] ?? '',
+    username: map['username'] ?? 'Anonymous',
+    content: map['content'] ?? '',
+    tags: List<String>.from(map['tags'] ?? []),
+    supportCount: map['supportCount'] ?? 0,
+    timestamp: (map['timestamp'] as Timestamp?)?.toDate() ?? DateTime.now(),
+    isReported: map['isReported'] ?? false,
+  );
+
+
+  Map<String, dynamic> toMap() => {
+    'userId': userId,
+    'username': username,
+    'content': content,
+    'tags': tags,
+    'supportCount': supportCount,
+    'timestamp': FieldValue.serverTimestamp(),
+    'isReported': isReported,
+  };
+}
